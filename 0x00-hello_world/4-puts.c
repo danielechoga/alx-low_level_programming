7e2b4e54fb92c02ce8c233d03109cda0c9cabb78
danielechoga <echogadanielabu@gmail.com>
2022-10-16 09:30:16 -0700

    C program Prints using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - This program prints using the put function
+ * put_prnt recieves the string to be printed
+ *
+ * Return: a non negative integer (0)
+ */
+int main(void)
+{
+	char put_prnt[] = "Programming is like building a multilingual puzzle";
+
+	puts(put_prnt);
+	return (0);
+}
