aa935482adbc8460638e5e1ac1822d77e25a426f
danielechoga <echogadanielabu@gmail.com>
2022-12-18 04:23:05 -0800

    function print 10 times the alphabet on new lines

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 	/* iterate through 10 lines*/
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
-		/* iterate throough alphabet */
+		/* iterate through alphabet */
 		for (j = 'a'; j <= 'z'; j++)
 		{
 			/* print to std output */
