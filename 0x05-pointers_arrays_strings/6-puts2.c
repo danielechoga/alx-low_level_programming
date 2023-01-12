97c5c49a5aa10101be536de42ab6b8e082893545
danielechoga <echogadanielabu@gmail.com>
2023-01-12 15:22:04 +0000

    print every other character of a string from the first character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,11 @@ void puts2(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str);
-		str = str + 2;
+		if (*str != '\0')
+		{
+			_putchar(*str);
+			str = str + 2;
+		}
 	}
 	_putchar('\n');
 }
