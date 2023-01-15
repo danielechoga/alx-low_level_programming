b95fb08e35b34c9ac1b2f52d28a72f4a0107cb62
danielechoga <echogadanielabu@gmail.com>
2023-01-15 11:39:33 +0000

    print the number of arguments passed into program

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program prints its name
+ * @argv: pointer to first character parameter
+ * @argc: parameter holding number of arguments
+ *
+ * Return: returns 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+	printf("%d\n", argc);
+	return (0);
+}
