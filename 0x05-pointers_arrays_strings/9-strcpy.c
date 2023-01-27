6486d1f842f9b64112c872fb24b4d874fc0ee4b9
danielechoga <echogadanielabu@gmail.com>
2023-01-27 00:11:47 +0000

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strcpy - function copies a string
+ * @dest: destination of copied string
+ * @src: pointer tp string to be copied
+ *
+ * Return: return the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		/*
+		 * store each string *(src + i) into
+		 * each space of dest + i
+		 */
+		*(dest + i) = *(src + i);
+
+		/*
+		 * terminate loop after null character has been
+		 * stored into dest + i
+		 */
+		if (*(src + i) != '\0')
+			i++;
+		else
+			i = -1;
+	}
+	return (dest);
+}
