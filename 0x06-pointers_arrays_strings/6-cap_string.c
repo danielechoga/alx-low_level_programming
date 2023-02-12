7b8a35dad4141f5f75585e991c24779980207c12
danielechoga <echogadanielabu@gmail.com>
2023-02-12 11:13:06 +0000

    capitalize all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,13 +9,26 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
+	/* captalize first character of string */
+	if (islower(str[0]))
+		str[0] -= 32;
+
+
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		/* identify word separator */
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+				|| str[i] == ',' || str[i] == ';' ||
+				str[i] == '.' || str[i] == '!' ||
+				str[i] == '?' || str[i] == '"' ||
+				str[i] == '(' || str[i] == ')' ||
+				str[i] == '{' || str[i] == '}')
 		{
+			/* capitalize character after word separator */
 			if (islower(str[i + 1]))
 				str[i + 1] -= 32; /* ASCII */
 		}
+
 		i++;
 	}
 
